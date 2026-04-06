0f1c393de2081ecd2d926b41bacb46ece6b4b137
Sujal Rajput <rajputsujal992@gmail.com>
2026-04-06 20:14:34 +0530

    Footer UI fix

--- skymart/src/components/Footer.jsx
+++ skymart/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-full fixed bottom-0 bg-[#0d0d0d] py-10 mt-10 border-t border-gray-900">
+    <footer className="w-full bg-[#0d0d0d] py-10 mt-10 border-t border-gray-900">
       <div className="flex flex-col items-center justify-center gap-2">
         {/* SkyMart Text */}
         <h2 className="text-[#d4ff00] text-xl font-bold tracking-tight">
