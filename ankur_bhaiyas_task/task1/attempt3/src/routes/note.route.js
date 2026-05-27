244280b5ef8806842b28198e433043bcb7715dcc
Sujal Rajput <rajputsujal992@gmail.com>
2026-05-27 23:01:21 +0530

    feat(api): add create note api

--- ankur_bhaiyas_task/task1/attempt3/src/routes/note.route.js
+++ ankur_bhaiyas_task/task1/attempt3/src/routes/note.route.js
@@ -1,5 +1,10 @@
 import express from "express";
+import { createNote } from "../controller/note.controller";
 
 const noteRouter = express.Router();
+
+noteRouter
+
+  .post("/create", createNote)
 
 export default noteRouter;
