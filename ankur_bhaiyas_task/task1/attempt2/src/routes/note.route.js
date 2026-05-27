cdc8a3a55f193f69e07eb265d872388126cc9b7d
Sujal Rajput <rajputsujal992@gmail.com>
2026-05-27 22:49:01 +0530

    feat(api): add get notes

--- ankur_bhaiyas_task/task1/attempt2/src/routes/note.route.js
+++ ankur_bhaiyas_task/task1/attempt2/src/routes/note.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createNote } from "../controller/note.controller";
+import { createNote, getNotes } from "../controller/note.controller";
 
 const noteRouter = express.Router();
 
 noteRouter
 
   .post("/create", createNote)
+  .get("/get", getNotes)
 
 export default noteRouter;
