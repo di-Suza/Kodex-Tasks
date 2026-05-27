1ccf7ae7a75d6b13c42d84433f1bf7bce082ddf3
Sujal Rajput <rajputsujal992@gmail.com>
2026-05-27 21:05:14 +0530

    feat(api): add create note api

--- ankur_bhaiyas_task/task1/attempt1/src/app.js
+++ ankur_bhaiyas_task/task1/attempt1/src/app.js
@@ -1,7 +1,31 @@
 import express from "express";
+import Notes from "./models/note.model.js";
 
 const app = express();
 app.use(express.json());
 
+// create note
+app.post("/api/notes", async (req, res) => {
+  const { title, description } = req.body;
+  fg;
+  // validation
+  if (
+    !title ||
+    !description ||
+    title.trim().length < 3 ||
+    description.trim().length < 10
+  ) {
+    return res.status(400).json({ error: "Invalid note data" });
+  }
+
+  // create note in database
+  const note = await Notes.create({ title, description });
+
+  return res.status(201).json({
+    success: true,
+    message: "Note created successfully",
+    note,
+  });
+});
 
 export default app;
