1e758f0f83e25d1dc729a72093a034bfe5bfef30
Sujal Rajput <rajputsujal992@gmail.com>
2026-05-28 18:30:56 +0530

    feat(api): add database config

--- ankur_bhaiyas_task/task2/server.js
+++ ankur_bhaiyas_task/task2/server.js
@@ -1,7 +1,10 @@
 const app = require("./src/app");
+const connectToDb = require("./src/configs/connectToDb");
 const port = process.env.PORT || 8080;
+require("dotenv").config();
 
 
+connectToDb();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
