50e15723039684f464a41cf77bd676cf6f99b132
Sujal Rajput <rajputsujal992@gmail.com>
2026-05-28 18:34:37 +0530

    feat(api): add note and auth route setup

--- ankur_bhaiyas_task/task2/src/app.js
+++ ankur_bhaiyas_task/task2/src/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
-
+const noteRouter = require("./routes/note.route");
+const authRouter = require("./routes/auth.route");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(express.json());
+
+// set routes
+app.use("api/notes", noteRouter);
+app.use("api/auth", authRouter);
 
 app.use(cookieParser());
 
