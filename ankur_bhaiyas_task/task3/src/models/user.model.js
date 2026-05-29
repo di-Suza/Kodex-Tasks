748e2060d0aa1bd86fda5424129c0f7fac01aec9
Sujal Rajput <rajputsujal992@gmail.com>
2026-05-30 01:28:43 +0530

    feat(api): add user schema

---
+++ ankur_bhaiyas_task/task3/src/models/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+// schema for user
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid email format"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+
+  },
+  { timestamps: true },
+);
+
+
+const Users = mongoose.model("users", userSchema);
+
+module.exports = Users;
