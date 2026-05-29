50a214e6787ef33e65b4b9648da61928da2f606c
Sujal Rajput <rajputsujal992@gmail.com>
2026-05-30 03:09:30 +0530

    feat(api): add upload image util

---
+++ ankur_bhaiyas_task/task3/src/utils/uploadImage.js
@@ -0,0 +1,12 @@
+import imagekit from "../configs/storage.js";
+
+export const uploadImageToImageKit = async (file, folder) => {
+  // Upload single image buffer to ImageKit
+  const uploadedImage = await imagekit.upload({
+    file: file.buffer.toString("base64"),
+    fileName: `${Date.now()}-${file.originalname}`,
+    folder,
+  });
+
+  return uploadedImage.url;
+};
