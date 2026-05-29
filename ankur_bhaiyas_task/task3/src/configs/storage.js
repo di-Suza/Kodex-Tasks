258f7fcc6dcb41f2402559683013f0010ab5276e
Sujal Rajput <rajputsujal992@gmail.com>
2026-05-30 01:40:04 +0530

    feat(api): add storage configs

---
+++ ankur_bhaiyas_task/task3/src/configs/storage.js
@@ -0,0 +1,9 @@
+import ImageKit from "imagekit";
+
+const imagekit = new ImageKit({
+  publicKey: process.env.IMAGE_KIT_PUBLIC,
+  privateKey: process.env.IMAGE_KIT_PRIVATE,
+  urlEndpoint: process.env.IMAGE_KIT_URL_ENDPOINT,
+});
+
+export default imagekit;
