1a39492c45a0365c064bc8c80826536c3a062f81
Sujal Rajput <rajputsujal992@gmail.com>
2026-05-30 01:22:49 +0530

    feat(api): add cache configs

---
+++ ankur_bhaiyas_task/task3/src/configs/cache.js
@@ -0,0 +1,17 @@
+import Redis from "ioredis";
+
+// redis configuration logic
+const redis = new Redis({
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD || undefined,
+  retryStrategy: (times) => {
+    return Math.min(times * 50, 2000);
+  },
+  maxRetriesPerRequest: null,
+});
+
+redis.on("connect", () => console.log("Redis Connected Successfully!"));
+redis.on("error", (err) => console.log("Redis Connection Error:", err));
+
+export default redis;
